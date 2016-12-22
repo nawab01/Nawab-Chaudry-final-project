76617e57d5c265dd60f9bc850fa0f327d7d23806
nawab01 <nawabchaudry@Nawabs-MBP.home>
2016-12-21 23:15:26 -0500

    update

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,61 +1,22 @@
 
-$('#enter').animate({
-	'margin-left': '53%'
-},2000)
-$('#image').animate({
-	'margin-left': '89%'
-},2000)
-var element = document.getElementById("start");
-$('#enter').on('click', function(){
-	apple();
-	$('#enter').animate({
-		'margin-left': '110%'
-	},1000)
-	$('#image').animate({
-		'margin-left': '190%'
-	},1000)
-	setTimeout(function() {
-		element.outerHTML = "";
-		delete element;
-	}, 1200)
-	$('#main').animate({
-		'margin-left': '10px'
-	},1000)
-})
-
-//image-slider
-
 function apple() {
-    //settings for slider
-    var width = 1200;
-    var animationSpeed = 1000;
-    var pause = 3000;
-    var currentSlide = 1;
-    //cache DOM elements
-    var $slider = $('#slider');
-    var $slideContainer = $('.slides', $slider);
-    var $slides = $('.slide', $slider);
-    var interval;
-
-    function startSlider() {
-        interval = setInterval(function() {
-            $slideContainer.animate({'margin-left': '-='+width}, animationSpeed, function() {
-                if (++currentSlide === $slides.length) {
-                    currentSlide = 1;
-                    $slideContainer.css('margin-left', 0);
-                }
-            });
-        }, pause);
-    }
-    function pauseSlider() {
-        clearInterval(interval);
-    }
-
-    $slideContainer
-        .on('mouseenter', pauseSlider)
-        .on('mouseleave', startSlider);
-
-    startSlider();
-
-
-}
+     $(".rslides").responsiveSlides({
+        auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false
+  nav: false,             // Boolean: Show navigation, true or false
+  random: false,          // Boolean: Randomize the order of the slides, true or false
+  pause: false,           // Boolean: Pause on hover, true or false
+  pauseControls: true,    // Boolean: Pause when hovering controls, true or false
+  prevText: "Previous",   // String: Text for the "previous" button
+  nextText: "Next",       // String: Text for the "next" button
+  maxwidth: "",           // Integer: Max-width of the slideshow, in pixels
+  navContainer: "",       // Selector: Where controls should be appended to, default is after the 'ul'
+  manualControls: "",     // Selector: Declare custom pager navigation
+  namespace: "rslides",   // String: Change the default namespace used
+  before: function(){},   // Function: Before callback
+  after: function(){}     // Function: After callback
+});
+  }
+  apple();
